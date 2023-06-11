b22f5a5d5bf38aeee9d6b92a18bcc37a335b323c
adity1999 <adity.kumar@collegedekho.com>
2023-06-11 11:32:16 +0530

    api setup

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = app.listen(PORT, console.log(`server started`.yellow.bold));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatapp-loh5.onrender.com",
+    origin: "https://chat-frontend-9vty.onrender.com/",
   },
 });
 
